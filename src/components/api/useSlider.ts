f94092feb7f253e0aedcb47da10a3255c0ac6278
s.aghabazaz@gmail.com <s.aghabazaz@gmail.com>
2025-01-13 13:18:18 +0330

    slider: update cache every 60 second
    Changelog: fix

--- src/components/api/useSlider.ts
+++ src/components/api/useSlider.ts
@@ -3,7 +3,14 @@ import useSWR from "swr";
 import { ISlider } from "./types";
 
 export function useSlider() {
-  const { data, error, isLoading } = useSWR(`/api/client/slider`, fetcher);
+  const { data, error, isLoading } = useSWR(
+      `/api/client/slider`,
+      fetcher,
+      {
+        refreshInterval: 60000, // Automatically refresh data every 60 seconds
+        revalidateOnFocus: true, // Revalidate on window focus
+      }
+  );
   return {
     data: data?.data || ([] as ISlider[]),
     isLoading,
