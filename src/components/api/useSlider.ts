8d42aad40e9fe9181b75324d8fa98e8ec529e7e5
s.aghabazaz@gmail.com <s.aghabazaz@gmail.com>
2025-01-13 14:49:29 +0330

    slider: fix text and location of slider
    Changelog: fix

--- src/components/api/useSlider.ts
+++ src/components/api/useSlider.ts
@@ -3,17 +3,17 @@ import useSWR from "swr";
 import { ISlider } from "./types";
 
 export function useSlider() {
-  const { data, error, isLoading } = useSWR(
-      `/api/client/slider`,
-      fetcher,
-      {
-        refreshInterval: 60000, // Automatically refresh data every 60 seconds
-        revalidateOnFocus: true, // Revalidate on window focus
-      }
-  );
-  return {
-    data: data?.data || ([] as ISlider[]),
-    isLoading,
-    isError: error,
-  };
+    const { data, error, isLoading } = useSWR(
+        `/api/client/slider`,
+        fetcher,
+        {
+            refreshInterval: 600, // Automatically refresh data every 60 seconds
+            revalidateOnFocus: true, // Revalidate on window focus
+        }
+    );
+    return {
+        data: data?.data || ([] as ISlider[]),
+        isLoading,
+        isError: error,
+    };
 }
